0efe6ae1d2c9386a3bb52292540250a2b6d5bdd7
yuri045 <emaildalavor@gmail.com>
2025-10-01 22:18:55 +0200

    Add crea_pdf() function to generate PDF files

---
+++ maker.py
@@ -0,0 +1,28 @@
+from fpdf import FPDF
+
+def crea_pdf(contenuto, nome_file="output.pdf"):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Verdana", size=12)
+    pdf.multi_cell(0, 10, contenuto)
+    pdf.output(nome_file)
+    print(f"\n PDF creato: {nome_file}")
+
+if __name__ == "__main__":
+    print(" Inserisci il testo da inserire nel PDF (premere due volte invio per terminare):\n")
+
+
+    righe = []
+    while True:
+        riga = input()
+        if riga == "":
+            break
+        righe.append(riga)
+
+    contenuto = "\n".join(righe)
+
+    nome_file = input("\n Inserisci il nome del file PDF da salvare senza estensione .pdf")
+    if not  nome_file.endswith(".pdf"):
+        nome_file += ".pdf"
+
+    crea_pdf(contenuto, nome_file)
