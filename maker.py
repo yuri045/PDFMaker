88f23cbf2466d5dc6bbd295b8a8656cc1cb5516c
yuri045 <emaildalavor@gmail.com>
2025-10-01 22:31:31 +0200

    Corretc errors for maker.py

--- maker.py
+++ maker.py
@@ -3,7 +3,7 @@ from fpdf import FPDF
 def crea_pdf(contenuto, nome_file="output.pdf"):
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Verdana", size=12)
+    pdf.set_font("Arial", size=12)
     pdf.multi_cell(0, 10, contenuto)
     pdf.output(nome_file)
     print(f"\n PDF creato: {nome_file}")
@@ -21,8 +21,8 @@ if __name__ == "__main__":
 
     contenuto = "\n".join(righe)
 
-    nome_file = input("\n Inserisci il nome del file PDF da salvare senza estensione .pdf")
+    nome_file = input("\n Inserisci il nome del file PDF da salvare senza estensione .pdf\n")
     if not  nome_file.endswith(".pdf"):
         nome_file += ".pdf"
 
-    crea_pdf(contenuto, nome_file)
+crea_pdf(contenuto, nome_file)
